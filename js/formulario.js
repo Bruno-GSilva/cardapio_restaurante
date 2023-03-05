bace7375c7a2168c257548df5d7cc2ec11acc613
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-03-05 19:06:09 -0300

    ajustes

--- js/formulario.js
+++ js/formulario.js
@@ -1,8 +1,11 @@
 const formRegister = document.getElementById("formRegister");
 const formLogin = document.getElementById("formLogin");
-const login = document.getElementById("login");
 const telaLogin = document.getElementById("telaLogin");
 const btn1 = document.getElementById("btn1");
+
+const login = document.getElementById("login");
+const password = document.getElementById("password");
+
 
 formRegister.setAttribute("content", "Faça seu registro agora!");
 btn1.innerHTML = "Registrar";
@@ -18,6 +21,7 @@ btn1.addEventListener("click", () => {
       formRegister.setAttribute("content", "Já tem conta faça seu login ?"));
 });
 
-formLogin.addEventListener('submit', () =>{
-  console.log(login.value)
+formLogin.addEventListener('submit', (e) =>{
+  e.preventDefault()
+  login.value === "adm" && password.value === "adm"?window.location.href = "http://127.0.0.1:5500/pages/dashboard.html":console.log("negado")
 })
