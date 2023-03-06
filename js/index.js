1c72e2d837c3d7cae2e668e7337e17b17313301d
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-03-06 09:09:36 -0300

    config

--- js/index.js
+++ js/index.js
@@ -5,5 +5,5 @@ carrinho.src = "https://cdn-icons-png.flaticon.com/512/6713/6713639.png"
 addproduto.setAttribute('href', carrinho)
 
 addproduto.addEventListener('click', ()=>{
-    console.log("adicinou")
+    alert("adicionado")
 })
